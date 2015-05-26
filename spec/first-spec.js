a100dca3238eb8f1873a7a1b5315d1a4277c6661
LukaszGajda <lgajda1@gmail.com>
2015-05-26 17:16:14 +0200

    oo

--- spec/first-spec.js
+++ spec/first-spec.js
@@ -1,19 +1,14 @@
 /* global browser */
 /// <reference path="../typings/jasmine/jasmine.d.ts"/>
+var assert = require('assert');
 var winston = require('winston');
 winston.add(winston.transports.File, { name: 'first-info-file', filename: './first-info.log', level: 'info' });
 describe('ThingWorx Test', function () {
     it('Checks if title is valid.', function (done) {
 		browser.url('http://lgajda:FtmRgUwmvCLj1ayYyFnE@46.242.130.58:8084/Thingworx/Runtime/index.html#mashup=AuditMashup')
-			.pause(20000)
+			.pause(10000)
 			.getTitle(function (err, title) {
-			if (err) {
-				winston.log('error', err);
-			}
-			else {
-				expect(title).toContain("Audit");
-				winston.log('info', title);
-			}
-		}).call(done);
+				assert(title.indexOf('Audit') != -1);
+				winston.log('info', title);}).call(done);
 	});
 });
--- spec/github-spec.js
+++ spec/github-spec.js
@@ -1,21 +1,16 @@
 /* global browser */
+var assert = require('assert');
 var winston = require('winston');
 winston.add(winston.transports.File, { name: 'github-info-file', filename: './github-info.log', level: 'info' });
 describe('Github test', function () {
     it('Checks if title contains the search query.', function (done) {
         browser.url('http://github.com')
-            .pause(15000)
+            .pause(7000)
             .setValue('[name="q"]', 'grunt-webdriver')
             .submitForm('.js-site-search-form')
-            .pause(15000)
-            .getTitle(function (err, title) {
-            if (err) {
-                winston.log('error', err);
-            }
-            else {
-                expect(title).toContain("grunt-webdriver");
-                winston.log('info', title);
-            }
-        }).call(done);
+            .pause(6000)
+            .getTitle(function(err,title) {
+                assert(title.indexOf('grunt-webdriver') !== -1);
+            }).call(done);
     });
 });
