baa9cf637c8909a35a01b61e026114f77335b44d
LukaszGajda <lgajda1@gmail.com>
2015-05-27 20:06:43 +0200

    add test with grunt

--- spec/first-spec.js
+++ spec/first-spec.js
@@ -13,11 +13,26 @@ describe('ThingWorx Test', function () {
 	});
 	
 	it('Check if you can fill form and click button', function(done){
-		browser.waitFor('#root_ContainedMashup-9_Textbox-9 input', 7000, function(err){
+		browser.url('http://lgajda:FtmRgUwmvCLj1ayYyFnE@46.242.130.58:8084/Thingworx/Runtime/index.html#mashup=AuditMashup')
+		.waitFor('#root_ContainedMashup-9_Textbox-9 input', 7000, function(err){
 			browser.setValue('#root_ContainedMashup-9_Textbox-9 input', 'test123');
 		})
 		.waitFor('#root_ContainedMashup-9_Button-8 button', 7000, function(err){
-			browser.click('#root_ContainedMashup-9_Button-8 button');
-		}).pause(7000).call(done);
+			browser.click('#root_ContainedMashup-9_Button-8 button', function(err){
+				browser.pause(7000).getAttribute('.tw-status-msg-box','style',function(err,attr){
+					var str_attr = attr.toString();
+					assert(str_attr.indexOf("display: none") == -1);
+					if(str_attr.indexOf("display: none") == -1)
+					{
+						browser.waitFor('.close-sticky-btn', 4000 , function(err){
+							browser.click('.close-sticky-btn').pause(2000).getAttribute('.tw-status-msg-box','style',function(err,attribute){
+								var attr_str = attribute.toString();
+								assert(attr_str.indexOf("display: none") > 0);
+							});
+						});
+					}
+				});
+			});
+		}).pause(3000).call(done);
 	});
 });
