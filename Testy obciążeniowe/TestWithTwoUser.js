5bd447a0692a93f630b144dc98f5476692284330
LukaszGajda <lgajda1@gmail.com>
2015-05-27 21:49:01 +0200

    add multiple test

--- Testy obciążeniowe/TestWithTwoUser.js
+++ Testy obciążeniowe/TestWithTwoUser.js
@@ -1,15 +1,8 @@
+/// <reference path="../typings/node/node.d.ts"/>
+var count = process.argv[2];
+var _count = parseInt(count);
 var child = require('child_process').exec;
-var one = child('node ./browserOne.js',function(error, stdout, stderr) {
-    console.log('stdout: ', stdout);
-    console.log('stderr: ', stderr);
-    if (error !== null) {
-        console.log('First process error: ', error);
-    }
-});
-var two = child('node ./browserTwo.js',function(error, stdout, stderr) {
-    console.log('stdout: ', stdout);
-    console.log('stderr: ', stderr);
-    if (error !== null) {
-        console.log('Second process error: ', error);
-    }
-});
+if (_count != NaN)
+    for (var i = 0; i < _count; i++) {
+        child('node ./browserOne.js');
+        }
