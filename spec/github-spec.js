be7f8d09811e7905ea51e26a9e4f9f3156077e52
LukaszGajda <lgajda1@gmail.com>
2015-06-11 22:23:52 +0200

    add next

--- spec/github-spec.js
+++ spec/github-spec.js
@@ -1,15 +1,25 @@
 /* global browser */
+//opis tych bibliotek w pliku first-spec
 var assert = require('assert');
 var winston = require('winston');
 winston.add(winston.transports.File, { name: 'github-info-file', filename: './github-info.log', level: 'info' });
+//Testy na stronie GitHuba
 describe('Github test', function () {
+    //sprawdz czy tytuł zawiera poszukiwana frazę
     it('Checks if title contains the search query.', function (done) {
+        //uruchom przeglądarkę ze strona GitHuba
         browser.url('http://github.com')
+            //odczekaj az sie zaladuje
             .pause(7000)
+            //ustaw wartość 'grunt-webdriver na elemencie o atrybucie 'name' równym 'q'
             .setValue('[name="q"]', 'grunt-webdriver')
+            //zatwiedź formularz
             .submitForm('.js-site-search-form')
+            //poczekaj az zalduje sie strona z wynikami
             .pause(6000)
+            //pobierz tytuł strony
             .getTitle(function(err,title) {
+                //sprawdź czy tytuł zawiera słowo 'grunt-webdriver'
                 assert(title.indexOf('grunt-webdriver') !== -1);
             }).call(done);
     });
--- spec/serialnet-spec.js
+++ spec/serialnet-spec.js
@@ -1,25 +1,35 @@
 /* global browser */
 var winston = require('winston');
 winston.add(winston.transports.File,{name: 'serialnet-info-file',filename: './seriale-info.log',level:'info'});
+//testujemy strone seriale.net
 describe('Serialnet.pl',function(){
+	//Test 1
+	//Sprawdx czy link do pomocy jest osiagalny
 	it('Check if link to help on menu is accessible.',function(done){
+		//zaladuj strone seriale.net w przeglądarce
 		browser.url('http://serialnet.pl')
+		//poczekaj aż pojawi się menu
 		.waitFor('#menu', 7000, function(err){
 			if(err)
 			{
+				//zapisz jesli sie nie powiodlo
 				winston.log('error','Can\'t find anything.');
 			}
 			else
 			{
+				//kliknij w link pomocy 
 				browser.click('#menu [href="http://serialnet.pl/pomoc/"]' , function(err)
 				{
 					if(err)
 					{
+						//nie ma w co kliknąć
 						winston.log('error','Can not find element.');
 					}
 					else
 					{
+						//kliknąłeś w link
 						winston.log('info','Find element.');
+						//zapisz w logu że wszystko przebiegło pomyslnie
 						expect(err).toBe(null);
 					}
 				});
