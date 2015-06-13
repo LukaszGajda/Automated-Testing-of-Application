286fbe42393349be7c887a96b71d06ba706740c8
LukaszGajda <lgajda1@gmail.com>
2015-06-13 15:22:33 +0200

    frisby

--- Testy REST/KpnSensor-01-spec.js
+++ Testy REST/KpnSensor-01-spec.js
@@ -4,3 +4,5 @@ var logon = config.Credentials.login + ':' + config.Credentials.password + '@';
 var config = JSON.parse(fs.readFileSync('spec/ThingWorxConfig.json','utf-8'));
 frisby.create('Check length of measure properties for KNPSensor')
 .get('http://' + logon + config.Thing +'KpnSensor-01/Services/GetMeasurementProperties?method=post&Accept=application/json')
+.expectJSONLenght("rows",11)
+.toss();
