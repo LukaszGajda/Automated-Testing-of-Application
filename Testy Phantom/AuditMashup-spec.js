cb6f87b3ea5b6ff3cd22897b09769ca0d58f2788
LukaszGajda <lgajda1@gmail.com>
2015-06-29 21:12:58 +0200

    xx

--- Testy Phantom/AuditMashup-spec.js
+++ Testy Phantom/AuditMashup-spec.js
@@ -7,17 +7,16 @@ var logon = config.Credentials.login + ':'
 	+ config.Credentials.password + '@';
 var url = "http://" + logon + config.Mashup + config.Mashups[0];
 page.open(url, function () {
-	var ua = page.includeJs("http://ajax.googleapis.com" +
-		"/ajax/libs/jquery/1.6.1/jquery.min.js", function () {
-			page.evaluate(function () {
-				var textbox = $("#root_ContainedMashup-9_Textbox-9 input");
-				textbox.val('test123');
-				var button = $(".close-sticky-btn");
-				button.click();
-				var messageBox = $(".tw-status-msg-box");
-				return messageBox.delay(5000).attr("style");
-			});
-			console.log(ua);
-			phantom.exit();
+	var ua = page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function () {
+		page.evaluate(function () {
+			var textbox = $("#root_ContainedMashup-9_Textbox-9 input");
+			textbox.val('test123');
+			var deleteButton = $("#root_ContainedMashup-9_Button-8 button");
+			deleteButton.click();
+			var messageBox = $(".tw-status-msg-box");
+			return messageBox.attr("style");
 		});
+        console.log(ua);
+        phantom.exit();
+	});
 });
