88019e3fecdf5ef5f9ac871cfbca3cce7a7b8135
LukaszGajda <lgajda1@gmail.com>
2015-06-29 21:10:35 +0200

    Revert "Revert "ddd""
    This reverts commit e04f83a2730392e29a72e583e9ce1d7b7d3ceb43.

--- Testy Phantom/AuditMashup-spec.js
+++ Testy Phantom/AuditMashup-spec.js
@@ -7,7 +7,7 @@ var logon = config.Credentials.login + ':'
 	+ config.Credentials.password + '@';
 var url = "http://" + logon + config.Mashup + config.Mashups[0];
 page.open(url, function () {
-	page.includeJs("http://ajax.googleapis.com" +
+	var ua = page.includeJs("http://ajax.googleapis.com" +
 		"/ajax/libs/jquery/1.6.1/jquery.min.js", function () {
 			page.evaluate(function () {
 				var textbox = $("#root_ContainedMashup-9_Textbox-9 input");
@@ -17,6 +17,7 @@ page.open(url, function () {
 				var messageBox = $(".tw-status-msg-box");
 				return messageBox.delay(5000).attr("style");
 			});
+			console.log(ua);
 			phantom.exit();
 		});
 });
